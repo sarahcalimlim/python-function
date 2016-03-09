d5c0e6124f9632753fd8ea4e00100d5977c79c0c
Sarah Gay Calimlim <sarahcalimlim@gmail.com>
2016-03-09 12:18:46 +0800

    changed function name from MyFunc to Funcout

--- function_stdoutput.py
+++ function_stdoutput.py
@@ -1,12 +1,12 @@
 #!/usr/bin/ env python
 
-def MyFunc():
+def FuncOut():
     name = raw_input ("Enter name:")
     age = input ("Enter age:")
 
     print "Hi! My name is", name + " and my age is", age
     print "Hi! My name is %s and my age is %d" %(name, age)
     print "Hi! My name is {} and my age is {}" .format(name, age)
-print MyFunc()
+print FuncOut()
 
 
